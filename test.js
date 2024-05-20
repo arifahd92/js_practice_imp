f602f1e30bd6c3fa29cfd51b6d646075e8baed51
md arif <arifahd92@gmail.com>
2024-05-20 14:44:14 +0530

    some advance topic in js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 /** @format */
-
+/*
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let key = arr[i];
@@ -21,3 +21,50 @@ let array = [12, 11, 13, 5, 6];
 console.log("Original array:", array);
 let sortedArray = insertionSort(array);
 console.log("Sorted array:", sortedArray);
+*/
+
+/*
+const user = {
+  // name: "arif",
+  email: "random@gmail.com",
+};
+const { name, email, address = "rtr" } = user;
+console.log(name);
+*/
+/*
+let obj = {
+  key1: "value1",
+  key2: "value2",
+};
+
+Object.prototype.inheritedKey = "inheritedValue"; // Adding an inherited property
+
+for (let key in obj) {
+  if (obj.hasOwnProperty(key)) {
+    console.log(key + ": " + obj[key]);
+  }
+}
+// Output:
+// key1: value1
+// key2: value2
+*/
+/*
+const obj = { a: 1, b: 2, c: 3 };
+
+const transformed = Object.fromEntries(
+  Object.entries(obj).map(([key, value]) => [key, value * 2]) //[ [a, 2],[b, 4], [c, 6]], Object.entries and Object.fromEntries has exactly apposite quality
+  //  Object.entries(obj), object to nested array, Object.fromEntries nested array to object
+);
+
+console.log(transformed);
+// Output: { a: 2, b: 4, c: 6 }
+*/
+
+let nestedArr = [
+  ["a", 1],
+  ["b", 2],
+  ["c", 3],
+];
+for (let [key, val] of nestedArr) {
+  console.log({ key, val }); //{ key: 'a', val: 1 } { key: 'b', val: 2 } { key: 'c', val: 3 }
+}
